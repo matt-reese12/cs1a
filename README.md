22ba49de7b68be480a78bff93fddf0f1d63f74f3
Matthew Reese <mreese4@saddleback.edu>
2023-08-21 17:53:01 -0700

    Updated README.md to reflect project state, and created p1.cpp. Reorganized homework file structure.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Naming Conventions:
+## TODO:
+[ ] Write README.md
+[ ] Add style guide
+[ ] Add helpful compile scripts
+[ ] Create resources to auto-upload and create pdfs, perhaps Twitter API (probably dead)
---
+++ homework/ch2/p1.cpp
@@ -0,0 +1,14 @@
+/* Sum of Two Numbers
+ *   Write a program that stores the integers 62 and 99 in variables, and stores the sum of
+ *   these two in a variable named total.
+ */
+#include <iostream>
+
+int main() {
+  int a = 62, b = 99;
+
+  int total = a + b;
+
+  // To test:
+  std::cout << "a + b = " << total << std::endl;
+}
